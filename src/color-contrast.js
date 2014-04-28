c541133aa0a5cc01777c54de962329f0f1b845f4
jeffersoncarpenter <jeffersoncarpenter2@gmail.com>
2014-04-28 13:52:00 -0500

    added semicolons and fixed indentation of new code

--- src/color-contrast.js
+++ src/color-contrast.js
@@ -14,14 +14,14 @@ angular.module('colorContrast', [])
     	var b = parseInt(hexcolor.substr(4,2),16);
     	var yiq = ((r*299)+(g*587)+(b*114))/1000;
     	return (yiq >= 128) ? DARK : LIGHT;
-    }
+    };
 
-	function stripNumberSign(color){
-		if(color[0] === "#") {
-			color = color.substring(1, color.length);
-		}
-		return color;
-    }
+    function stripNumberSign(color){
+        if(color[0] === "#") {
+            color = color.substring(1, color.length);
+        }
+        return color;
+    };
     
     return {
       restrict: 'A',
